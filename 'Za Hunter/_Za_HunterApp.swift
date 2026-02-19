db6b461a8accf3cb731dc843f310040152528f1e
Edwin Tovar <poohwinnie2800@gmail.com>
2026-02-19 08:59:56 -0800

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Edwin Tovar on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
